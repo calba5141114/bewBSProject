b805856e19385eb571b6e15e19fb92b66197f36b
Carlos Alba <calba5141114@Carloss-MacBook-Pro.local>
2018-09-20 17:40:51 -0700

    init

---
+++ scripts/index.js
@@ -0,0 +1,9 @@
+ScrollReveal().reveal('.img-fluid');
+ScrollReveal().reveal('.tbr');
+
+// alert('Hello World');
+
+iziToast.show({
+    title: 'Inc Company Co',
+    message: 'We were expecting you.'
+});
